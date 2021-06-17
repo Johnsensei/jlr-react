209b88a4677a4aebd3afcddd5b4adce1eb0c637a
Johnsensei <john@johnsensei.com>
2021-06-17 08:54:45 -0500

    Added colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
-import { CLASSES } from './components/shared/languageclasses';
+import { CLASSES } from './components/shared/classes';
 import './App.css';
 
 class App extends Component {
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
       return (
           <div className="App">
-              <Navbar dark color="primary">
+              <Navbar dark color="#B9936D">
               <div className="container">
                 {/* TODO: Replace with logo image. */}
                   <NavbarBrand href="/">Japanese Language Room</NavbarBrand>
--- src/components/shared/classes.js
+++ src/components/shared/classes.js
@@ -2,7 +2,7 @@ export const CLASSES = [
     {
         id: 0,
         name: 'Japanese 1.0',
-        image: 'assets/images/hiragana.png',
+        image: 'assets/images/hiragana2.png',
         description: "The class for total beginners where we’ll learn:",
         content: ["Hiragana", "Greetings", "Self-introductions", "Counting to 99,999", "Money", "Asking where", "Ordering food"],
         comments: [
@@ -46,7 +46,7 @@ export const CLASSES = [
     {
         id: 1,
         name:'Japanese 1.1',
-        image: 'assets/images/katakana.png',
+        image: 'assets/images/katakana2.png',
         description: "Level up by learning:",
         content: ["Katakana", "Telling time", "Asking when", "Verbs", "Negative Verbs", "Planning a trip", "Jobs", "Invitations"],
         comments: [
@@ -90,7 +90,7 @@ export const CLASSES = [
     {
         id: 2,
         name:'Japanese 1.2',
-        image: 'assets/images/kanji.png',
+        image: 'assets/images/kanjiBIG1.png',
         description: "Go farther with:",
         content: ["Kanji", "Compound words", "Numbers in millions", "Asking why", "Giving reasons", "Paste tense verbs", "Asking directions", "Getting help"],
         comments: [
@@ -134,7 +134,6 @@ export const CLASSES = [
     {
         id: 3,
         name:'Japanese 1.3',
-        // TODO: set this class-session image to the same size as kanji etc.
         image: 'assets/images/class-session.png',
         description: "Expand your conversional abilities with:",
         content: ["20 new Kanji", "Present Progressive Verbs", "Verbs of Being", "Adjectives", "Adverbs", "Making a Schedule", "Communicating with Coworkers", "Family"],
