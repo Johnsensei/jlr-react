da1acf21a5a9cb6cfd5c4b4e3c6e8632cea134f2
Johnsensei <john@johnsensei.com>
2021-06-18 14:26:06 -0500

    Custom font added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'typeface-lobster';
+import './fonts/AdelonBold.ttf'
 import 'typeface-open-sans';
 import App from './App';
 
