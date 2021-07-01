c8a3ec780dcb90ace918eee9cddb260fb041323b
Johnsensei <john@johnsensei.com>
2021-06-30 19:17:59 -0500

    Google Button Added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,8 @@
 import React, { Component }  from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+// import GoogleBtn from './GoogleBtn';
+import GoogleButton from 'react-google-button'
 
 class Header extends Component{
 
@@ -38,7 +40,7 @@ class Header extends Component{
                         {/* <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand> */}
                             <NavbarToggler onClick={this.toggleNav} />
                             <Collapse isOpen={this.state.isNavOpen} navbar>
-                                <Nav navbar>
+                                <Nav navbar className="navbar-nav container-lg">
                                     <NavItem>
                                         <NavLink className="nav-link" to="/home">
                                             <i className="fa fa-home fa-lg" /> Home
@@ -63,6 +65,9 @@ class Header extends Component{
                                         <NavLink className="nav-link" to="/contactus">
                                             <i className="fa fa-address-card fa-lg" /> Contact Us
                                         </NavLink>
+                                    </NavItem>
+                                    <NavItem className="ms-auto">
+                                        <GoogleButton  onClick={() => { alert('Google button clicked') }}/>
                                     </NavItem>
                                 </Nav>
                             </Collapse>
