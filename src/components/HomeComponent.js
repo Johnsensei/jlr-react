3a7a440caef41b6a050f0663d551277769ce01f4
Johnsensei <john@johnsensei.com>
2021-07-08 17:29:09 -0400

    Responsive Video on Home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,8 +3,29 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { CardMedia } from '@material-ui/core'
 
 function RenderCard({item}){
-    const videoItem = <CardMedia component="iframe" title="test" height="240" muted="false" src={item.video} />;
+    
     const imageItem = <CardImg src={item.image} alt={item.name} />;
+    const videoItem = <div
+    className="video"
+    style={{
+      position: "relative",
+      paddingBottom: "56.25%" /* 16:9 */,
+      paddingTop: 25,
+      height: 0
+    }}
+  >
+    <iframe
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%"
+      }}
+      src={item.video}
+      frameBorder="0"
+    />
+  </div>
 
     return(
         <Card>
@@ -21,13 +42,13 @@ function Home (props){
     return(
         <div className="container">
             <div className="row">
-                <div className="col-md m-1">
+                <div className="col-xl m-1">
                     <RenderCard item={props.languageClass} />
                 </div>
-                <div className="col-md m-1">
+                <div className="col-xl m-1">
                     <RenderCard item={props.app} />
                 </div>
-                <div className="col-md m-1">
+                <div className="col-xl m-1">
                     <RenderCard item={props.video} />
                 </div>
             </div>
